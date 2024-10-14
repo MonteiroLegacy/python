2452ab5928ef38c37125688a4f3661e7bfc94dbc
davi <davimonteirodelucena@gmail.com>
2024-10-14 19:35:39 -0300

    Milimetros para polegadas

---
+++ Energia.py
@@ -0,0 +1,5 @@
+Valordosalariominimo = float(input("digite quantidade do salario minimo: "))
+QuantidadeConsumidaDeEnergia = float(input("digite quantidade de KW consumidos: "))
+valorUnidadeKw = Valordosalariominimo * 0.7 / 100;
+ValorTotal = valorUnidadeKw * QuantidadeConsumidaDeEnergia
+print("valor total", ValorTotal)
