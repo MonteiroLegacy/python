daa1d2aab283cc93796dd6a64a1462ddf4a84050
davi <davimonteirodelucena@gmail.com>
2024-10-14 20:09:39 -0300

    Montando decimal

--- Energia.py
+++ Energia.py
@@ -1,6 +1,5 @@
 Valordosalariominimo = float(input("digite quantidade do salario minimo: "))
 QuantidadeConsumidaDeEnergia = float(input("digite quantidade de KW consumidos: "))
-valorUnidadeKw = Valordosalariominimo * 0.7 / 100;
+valorUnidadeKw = Valordosalariominimo * 0.7 / 100
 ValorTotal = valorUnidadeKw * QuantidadeConsumidaDeEnergia
 print("valor total", ValorTotal)
-1
---
+++ Matematica.py
@@ -0,0 +1,8 @@
+
+N1 = float(input("Digite uma centena:"))
+N2 = float(input("Digite uma dezena:"))
+
+N3 = float(input('Digite uma unidade:'))
+N4 = N1 * 100 + N2 * 10 + N3
+Quadrado = N4 * N4
+print ("O resultado e o quadrado são respectivamente", N4, Quadrado )
