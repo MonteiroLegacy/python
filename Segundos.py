4fe3c655a90c21cfc9cd715a1fe44ecaa6b598be
davi <davimonteirodelucena@gmail.com>
2024-10-14 20:20:09 -0300

    Contador de segundos

---
+++ Segundos.py
@@ -0,0 +1,5 @@
+ValorEmHoras = float(input("digite o valor em horas:"))
+ValorEmMinutos = float(input("digite o valor em minutos:"))
+ValorEmSegundos = float(input("digite o valor em segundos:"))
+SegTotal = ValorEmHoras * 3600 + ValorEmMinutos * 60 + ValorEmSegundos
+print("A quantidade de segundos total e:", SegTotal)
