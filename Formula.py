54f606d34a5d63ab7832682e2e45a43a2745ea7d
davi <davimonteirodelucena@gmail.com>
2024-10-15 09:25:23 -0300

    Calculador de Matriz

---
+++ Formula.py
@@ -0,0 +1,7 @@
+
+A = float(input("Insira A: "))
+B = float(input("Insira B: "))
+C = float(input("Insira C: "))
+D = float(input("Insira D: "))
+Determinante = (A * D) - (B * C)
+print ("A determinante e igual a: ", Determinante)
