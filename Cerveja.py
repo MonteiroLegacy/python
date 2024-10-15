91b2d0725a2dcc865b46b1a887e12e3bdac179ff
davi <davimonteirodelucena@gmail.com>
2024-10-15 10:10:22 -0300

    Calculadora de preço por lata de cerveja

---
+++ Cerveja.py
@@ -0,0 +1,8 @@
+
+π= 3.14159
+Raio= float(input('Raio: '))
+Altura = float(input('Altura: '))
+AreaLateral = (2 *π) * (Altura) * (Raio)
+ValorPorLata = AreaLateral * 100
+print ("valor total e", ValorPorLata)
+
