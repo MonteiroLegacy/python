34cecf4ca680187cc4efa1d14d27e24fe61f969a
Fábio Nogueira de Lucena <kyriosdata@ufg.br>
2024-10-18 14:17:04 -0300

    ex12

--- ex 12.py
+++ ex 12.py
@@ -1,4 +1,13 @@
-CustodeFabrica= float(input("digite o custo de fabrica"))
+
+while(True):
+    try:
+        CustodeFabrica = float(input("digite o custo de fabrica"))
+        break
+    except ValueError:
+        print("Digite um valor valido")
+
+
+
 PorcentagemDistribuidor= float(input("digite a porcentagem do distribuidor"))
 PorcentagemImpostos= float(input("digite a porcentagem dos impostos"))
 CustoConsumidor= CustodeFabrica + (CustodeFabrica * PorcentagemDistribuidor / 100) + (CustodeFabrica * PorcentagemImpostos / 100)
