84f2d4f0648f0a89d76eb04a4e33e96009cb3e22
davi <davimonteirodelucena@gmail.com>
2024-10-18 19:14:33 -0300

    Respostas

---
+++ EX18.py
@@ -0,0 +1,8 @@
+N1 =  float(input("primeira nota"))
+N2 = float(input("segunda nota"))
+N3 = float(input("terceira nota"))
+Media = (N1 + N2 + N3) / 3
+if  Media > 5:
+ print ("Aprovado")
+else:
+ print ("Reprovado")
