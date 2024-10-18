2cb9360ffc4d30d27b50450a0eda9a56e9668f70
Fábio Nogueira de Lucena <kyriosdata@ufg.br>
2024-10-18 14:00:20 -0300

    ex12

---
+++ ex 12.py
@@ -0,0 +1,5 @@
+CustodeFabrica= float(input("digite o custo de fabrica"))
+PorcentagemDistribuidor= float(input("digite a porcentagem do distribuidor"))
+PorcentagemImpostos= float(input("digite a porcentagem dos impostos"))
+CustoConsumidor= CustodeFabrica + (CustodeFabrica * PorcentagemDistribuidor / 100) + (CustodeFabrica * PorcentagemImpostos / 100)
+print("O custo do consumidor e:", CustoConsumidor)
